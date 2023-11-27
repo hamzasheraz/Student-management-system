f06f29ea53c7d524341b97ae5baef78ee1a28665
Rafay-Dil123 <dilrafay@gmail.com>
2023-11-27 12:18:39 +0500

    added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Student from "./Components/Student";
 function App() {
   return (
     <div className="App">
+      <div>oewiurss</div>
       <Routes>
         <Route path="/" element={<Navigate to="/student-login" />} />
         <Route path="/student-login/*" element={<Home />} />
