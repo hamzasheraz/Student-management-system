55489893976f0cf9599b349afbe863cfb4917de0
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-08-29 00:40:25 +0500

    domain setting

--- smsbackend/smsbackend/settings.py
+++ smsbackend/smsbackend/settings.py
@@ -15,10 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = [ 'flex1-7tzwawqy.b4a.run',
-    'localhost',
-    '127.0.0.1',
-    'flexmanagement-feyaty98.b4a.run']
+CSRF_TRUSTED_ORIGINS = [
+    'localhost',]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
